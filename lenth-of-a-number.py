0d03629947ffcd2cebb9f50a78d97f3619360904
coderabhishek2060 <112436954+coderabhishek2060@users.noreply.github.com>
2023-06-20 21:40:05 +0530

    Add files via upload

---
+++ lenth-of-a-number.py
@@ -0,0 +1,9 @@
+##Counting the length of a number
+# a=int(input("Enter a Number to check its length"))
+# count=0
+# x=a
+# while x>0 :
+#   x=x/10
+#   x=int(x)
+#   count=count+1
+# print("Length of given number is : ",count)
